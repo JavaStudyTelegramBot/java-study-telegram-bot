06eef240c81fc1be587338dea93e2bc23a57fce8
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-04-17 17:00:25 +0300

    JSTB-0: created SpringBoot skeleton

---
+++ src/main/java/com/github/javastudytelegrambot/jstb/JavaStudyTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.javastudytelegrambot.jstb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyTelegramBotApplication.class, args);
+	}
+
+}
